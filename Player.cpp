c1b3c5d1d88375beefbc31fbc813189312cadee5
einav242 <93386470+einav242@users.noreply.github.com>
2022-04-26 12:36:30 +0300

    Add files via upload

---
+++ Player.cpp
@@ -0,0 +1,24 @@
+#include "Player.hpp"
+using namespace coup;
+using namespace std;
+
+Player::~Player()
+{
+}
+void Player::income()
+{
+}
+void Player::foreign_aid()
+{
+}
+void Player::coup(Player const &p)
+{
+}
+string Player::role()
+{
+    return "";
+}
+int Player::coins()
+{
+    return 0;
+}
